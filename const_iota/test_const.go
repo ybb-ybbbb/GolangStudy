dbad327a8db8fc82e26d45910329688135836671
ybb <547392090@qq.com>
2025-04-23 16:18:35 +0800

    2

--- const_iota/test_const.go
+++ const_iota/test_const.go
@@ -5,11 +5,15 @@ import (
 )
 
 const (
-	a = iota
-	b
-	c
+	a = iota       //iota是一个关键字（只能配合const使用），逐行递增1，iota=a=0
+	b              //iota=b=1
+	c = iota*2 + 1 //iota=2,c=2*2+1
+	d              //iota=3,d=3*2+1
 )
 
 func main() {
 	fmt.Println("a=", a)
+	fmt.Println("b=", b)
+	fmt.Println("c=", c)
+	fmt.Println("d=", d)
 }
